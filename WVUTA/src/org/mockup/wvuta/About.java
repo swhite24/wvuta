c459c7e98ee326dc9569ec426702b86672589732
Steven D White <swhite24@813esb021.csee.wvu.edu>
2012-04-17 21:17:21 -0400

    changed app to not be debugggable

--- WVUTA/src/org/mockup/wvuta/About.java
+++ WVUTA/src/org/mockup/wvuta/About.java
@@ -5,13 +5,11 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class About extends Activity{
-	private static final String TAG = "WVUTA::ABOUT";
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
 		
-		Log.d(TAG, "AboutActivity onCreate");
 	}
 
 } 
