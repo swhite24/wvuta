7f5df831173253fe04e65b675ec19e0b6be53810
Steven White <Steven.White@ensequence.com>
2012-07-17 22:21:15 -0400

    Initial commit: new wvuta

---
+++ src/org/mockup/wvuta/Main.java
@@ -0,0 +1,21 @@
+package org.mockup.wvuta;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Main extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
