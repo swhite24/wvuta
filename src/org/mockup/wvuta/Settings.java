9f004e7381ee18e8751d715efc356ec0822d1536
Steven White <Steven.White@ensequence.com>
2012-07-18 21:54:22 -0400

    Settings: added unused suppresswarnings

--- src/org/mockup/wvuta/Settings.java
+++ src/org/mockup/wvuta/Settings.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class Settings extends PreferenceActivity {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
